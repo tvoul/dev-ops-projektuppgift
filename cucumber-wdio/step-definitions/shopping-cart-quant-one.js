32bf9a5086d2b81f26a47672c380eb58a99914bf
MariaFranz <maria.franz@hotmail.com>
2022-04-22 10:32:19 +0200

    revert

--- cucumber-wdio/step-definitions/shopping-cart-quant-one.js
+++ cucumber-wdio/step-definitions/shopping-cart-quant-one.js
@@ -1,13 +1,21 @@
 const { Given, When, Then } = require('@wdio/cucumber-framework');
 const pauseTime = 50;
 
-Given('that I can see the product list', async () => {
+Given('that I am logged in', async () => {
   await browser.url('/');
-  await browser.pause(pauseTime);
+  let loginHref = await $('#loginHref')
+  await loginHref.click()
+  let email = await $('#email')
+  await email.setValue('user@gmail.com')
+  let password = await $('#password')
+  await password.setValue('12345678')
+  await $('#loginBtn').click()
+  browser.pause(pauseTime)
 });
 
 When(/^I click on the buy button for "(.*)"$/, async (productName) => {
-  await browser.pause(pauseTime);
+  browser.pause(pauseTime)
+  await browser.url('/');
   let products = await $$('.productInList');
   let foundProduct;
   for (let product of products) {
@@ -15,19 +23,24 @@ When(/^I click on the buy button for "(.*)"$/, async (productName) => {
       foundProduct = product;
     }
   }
+  browser.pause(pauseTime)
   expect(foundProduct).toBeTruthy();
   let buyButton = await foundProduct.$('.buyButton');
   await buyButton.scrollIntoView();
   await buyButton.click();
-  await browser.pause(pauseTime);
 });
 
 Then(/^(\d*) item of "(.*)" should be added to the cart$/, async (quantity, productName) => {
-  await browser.pause(pauseTime);
   let tds = await $$('.shoppingCart tr:first-child td');
   await expect(tds[0]).toHaveText(quantity);
   await expect(tds[1]).toHaveText(productName);
   await tds[0].scrollIntoView();
+<<<<<<< HEAD
   await browser.pause(pauseTime);
 });
 
+=======
+});
+
+//trigger actions
+>>>>>>> parent of 655e4d0 (Reverting to when WDIO worked)
