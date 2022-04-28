129ae82e519e3b09b002c22ec59646b2fb34ee03
Mia <mialatvala@yahoo.com>
2022-04-28 16:45:10 +0200

    made use of helper for code consistency

--- frontend/js/registration.js
+++ frontend/js/registration.js
@@ -23,8 +23,7 @@ function renderRegistrationForm() {
   `;
 }
 
-
-document.querySelector('body').addEventListener('submit', async (event) => {
+grabEl('body').addEventListener('submit', async (event) => {
 
   let target = event.target;
 
@@ -57,7 +56,7 @@ document.querySelector('body').addEventListener('submit', async (event) => {
   catch (ignore) { }
 
   if (!result.changes) {
-    document.querySelector('.register').innerHTML = `
+    grabEl('.register').innerHTML = `
       <h3>Something went wrong!</h3>
       <p>We could not register you right now because of a technical problem.</p>
       <p>Please try again later!</p>
@@ -65,23 +64,20 @@ document.querySelector('body').addEventListener('submit', async (event) => {
     return;
   }
 
-  document.querySelector('.register').innerHTML = `
+  grabEl('.register').innerHTML = `
     <h3>Welcome as a customer!</h3>
     <p>You are now successfully registered. Have a magical time shopping!</p>
   `;
 });
 
-document.querySelector('body').addEventListener('click', (event) => {
+grabEl('body').addEventListener('click', (event) => {
 
   if (!event.target.closest('a[href="/register"]')) { return; }
 
   event.preventDefault();
 
-  let registerDiv = document.querySelector('.register');
+  let registerDiv = grabEl('.register');
   registerDiv.innerHTML = renderRegistrationForm();
   registerDiv.classList.remove('hidden');
-  document.querySelector('.modal-hider').classList.remove('hidden');
+  grabEl('.modal-hider').classList.remove('hidden');
 });
-
-
-
