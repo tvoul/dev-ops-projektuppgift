9a324313968b8a2f2f195f5a4e26ec9520aaad0b
tomasvoulgaris <90894009+tvoul@users.noreply.github.com>
2022-04-29 14:21:57 +0200

    Revert word wrap and minor wdio fixes

--- cucumber-wdio/step-definitions/shopping-cart-delete-button.js
+++ cucumber-wdio/step-definitions/shopping-cart-delete-button.js
@@ -6,9 +6,9 @@ Given(/^that I have put a "(.*)" in my cart$/, async (productName) => {
   while (!(await $('.productInList'))) {
     browser.pause(100);
   }
-  let products = await $$('.productInList');
   let foundProduct;
   while (typeof foundProduct === 'undefined'){
+    let products = await $$('.productInList');
     for (let product of products) {
       if ((await product.getText()).includes(productName)) {
         foundProduct = product;
