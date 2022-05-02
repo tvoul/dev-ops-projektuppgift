f22d6378ef236a4dcfe036b3909c812f433ab23e
tomasvoulgaris <90894009+tvoul@users.noreply.github.com>
2022-05-02 13:51:29 +0200

    Removed scrollIntoView from all wdio tests

--- cucumber-wdio/step-definitions/shopping-cart-delete-button.js
+++ cucumber-wdio/step-definitions/shopping-cart-delete-button.js
@@ -17,7 +17,6 @@ Given(/^that I have put a "(.*)" in my cart$/, async (productName) => {
   }
   expect(foundProduct).toBeTruthy();
   let buyButton = await foundProduct.$('.buyButton');
-  await buyButton.scrollIntoView();
   await buyButton.click();
 });
 
@@ -26,7 +25,6 @@ When(/^I click on the delete button on "(.*)"$/, async (productName) => {
     browser.pause(100);
   }
   let tds = await $$('.shoppingCart tr:first-child td');
-  await tds[4].scrollIntoView();
   let deleteButton = await tds[4].$('.deleteButton')
   await deleteButton.click()
 });
@@ -39,7 +37,6 @@ Then('the total amount due should be 0', async () => {
   while (!(await tds)) {
     browser.pause(100);
   }
-  await tds[1].scrollIntoView();
   let total = await tds[1].getText()
   await expect(total).toBe('0,00 kr')
 });
